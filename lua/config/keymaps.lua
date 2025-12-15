50fe3863b11353bac8b65c8e68bb76521028ae56
LionelRohner <lionel.rohner@gmail.com>
2025-12-15 14:50:00 -0500

    add keymap for disabling LSP
    * Warnings are quite annoying in md, when working in quarto files

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+--- Quarto Stuff
 local quarto = require("quarto")
 quarto.setup()
 vim.keymap.set("n", "<leader>öp", quarto.quartoPreview, { silent = true, noremap = true, desc = "QuartoPreview" })
+
+--- Disable LSP Diagnostic disabling
+vim.keymap.set("n", "<leader>td", function()
+  vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+end, { silent = true, noremap = true, desc = "Toggle LSP-Diagnostic" })
