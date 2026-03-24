6a8a601cee230185a355a4836237bf088ea44657
LionelRohner <lionel.rohner@gmail.com>
2026-03-24 11:45:06 -0400

    test ruff linter for python

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,5 +5,8 @@
 -- Python LSP base >> customise lua/plugins/extend-lsp.lua
 vim.g.lazyvim_python_lsp = "basedpyright"
 
+-- Ruff Python linter
+vim.g.lazyvim_python_ruff = "ruff"
+
 -- Set to false to disable auto format
 vim.g.lazyvim_eslint_auto_format = true
