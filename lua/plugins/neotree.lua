5d9f0659b67993e600e42a036095aa1ddbfb2d48
LionelRohner <lionel.rohner@gmail.com>
2026-03-24 11:45:18 -0400

    add neotree

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself,
+    opts = {
+      close_if_last_window = true,
+      window = {
+        position = "right",
+      },
+    },
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        always_show = {
+          ".env",
+        },
+      },
+    },
+  },
+}
